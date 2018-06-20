05c75cbc2789dfc5631197a093e9b51ad8a96b15
Sandeep Baikan <sandeep.baikan@coditas.com>
2018-06-20 19:28:43 +0530

    Assignment 1:Q-5 (Change app.component.ts inline html template)

---
+++ Assignment_1/Q-5/FirstApp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div style="text-align:center">
+    <h1>
+      Marvellous Infosystems
+      </h1>
+  </div>
+  <h3 style="font-style:italic;text-align:center">Educating for better tomorrow.</h3>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
